8684fc102af9cd7607dca340d9915c48d107523e
Edible <rwoznicki@edu.cdv.pl>
2023-06-24 18:14:48 +0200

    Added fade out to money gain

--- Assets/Antek Sandobx/PopUpControelr.cs
+++ Assets/Antek Sandobx/PopUpControelr.cs
@@ -24,11 +24,30 @@ public class PopUpControelr : MonoBehaviour
     }
 
     IEnumerator TextFade()
-    {   
-        yield return new WaitForSeconds(timer);
-        popUp.enabled = false;
-        StopCoroutine(TextFade());
-        yield return null;
-     
+    {
+        Color originalColor = popUp.color; // Store the original color of the TextMeshProUGUI component
+
+        float elapsedTime = 0f; // Initialize the elapsed time
+
+        while (elapsedTime < timer)
+        {
+            elapsedTime += Time.deltaTime; // Increase the elapsed time by the time passed since the last frame
+
+            // Calculate the current alpha value based on the elapsed time and the fade duration
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / timer);
+
+            // Set the alpha value in the vertex color
+            Color fadedColor = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            popUp.color = fadedColor;
+
+            yield return null;
+        }
+
+        // Ensure the alpha value is set back to 1 when the fade out is complete
+        Color fullyOpaqueColor = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
+        popUp.color = fullyOpaqueColor;
+
+        popUp.enabled = false; // Disable the TextMeshProUGUI component after the fading is complete
     }
+
 }
