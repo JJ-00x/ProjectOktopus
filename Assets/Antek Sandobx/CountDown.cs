dbf342520e020fb331c7495e3147017e5b1fdcb8
antek1085 <antek1085@o2.pl>
2023-06-25 18:49:02 +0200

    last update

--- Assets/Antek Sandobx/CountDown.cs
+++ Assets/Antek Sandobx/CountDown.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CountDown : MonoBehaviour
 {
     // Start is called before the first frame update
     public float timeRemaining = 60;
     public bool timerIsRunning = false;
+    public TextMeshProUGUI canvastext;
     public GameObject canvas;
+    public ScriptableObjectINT money;
+    public TextMeshProUGUI moneytext;
     private void Start()
     {
         // Starts the timer automatically
@@ -20,13 +25,19 @@ public class CountDown : MonoBehaviour
             if (timeRemaining > 0)
             {
                 timeRemaining -= Time.deltaTime;
+                canvastext.text = timeRemaining.ToString() + "s";
+                
             }
             else
             {
+                
                 canvas.SetActive(true);
                 timeRemaining = 0;
                 timerIsRunning = false;
+                SceneManager.LoadScene(11);
             }
         }
+
+        moneytext.text = money.value.ToString() + "$";
     }
 }
