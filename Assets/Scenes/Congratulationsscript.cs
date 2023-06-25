292a0e2a80f77307d3ab6fbddf0cff93091fce90
antek1085 <antek1085@o2.pl>
2023-06-25 18:14:51 +0200

    Congratulation

---
+++ Assets/Scenes/Congratulationsscript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Congratulationsscript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI money;
+
+    [SerializeField] private ScriptableObjectINT playerMoney;
+    // Start is called before the first frame update
+    void Start()
+    {
+        money.text = playerMoney.value.ToString() + "$";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
